00c90d0788f91beff9be22a7a5493c598a424f78
Siva Bhavana Dachepalli <sivabhavanad@gmail.com>
2026-08-07 13:38:40 +0530

    Update document classifier

--- utils/classifier.py
+++ utils/classifier.py
@@ -1,43 +1,24 @@
-import re
-
-
 def classify_document(text):
 
     text = text.lower()
 
-    if any(word in text for word in [
-        "invoice",
-        "gst",
-        "invoice number",
-        "tax invoice",
-        "subtotal",
-        "total amount"
-    ]):
+    if "invoice" in text:
         return "Invoice"
 
-    elif any(word in text for word in [
-        "resume",
-        "education",
-        "experience",
-        "skills",
-        "projects"
-    ]):
+    elif "resume" in text:
         return "Resume"
 
-    elif "government of india" in text and "aadhaar" in text:
+    elif "aadhaar" in text:
         return "Aadhaar Card"
 
-    elif "income tax department" in text and "permanent account number" in text:
+    elif "pan" in text:
         return "PAN Card"
 
     elif "passport" in text:
         return "Passport"
 
-    elif any(word in text for word in [
-        "receipt",
-        "cash receipt",
-        "payment received"
-    ]):
-        return "Receipt"
+    elif "bank" in text:
+        return "Bank Statement"
 
-    return "Unknown Document"
+    else:
+        return "Unknown Document"
